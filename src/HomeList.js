82ddcc1b794ad7554d2bdae651d732bd0dbbd72b
Andras Csepreghy <acsepreghy@ganintegrity.com>
2017-06-15 21:38:43 +0200

    Removed clutter, gave basic structure

---
+++ src/HomeList.js
@@ -0,0 +1,19 @@
+import React, {Component} from 'react';
+
+class HomeList extends Component {
+
+  constructor(props) {
+    super();
+
+  }
+
+  render() {
+    return (
+      <div>
+        <h1>Hello World</h1>
+      </div>
+    );
+  }
+}
+
+export default HomeList;
