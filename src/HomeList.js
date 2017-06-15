bf8a2a158865b5fcd443c6b048b32ba45f0b4ad0
Andras Csepreghy <acsepreghy@ganintegrity.com>
2017-06-15 21:54:11 +0200

    Fetch JSON from public with axios, assingn it to state

--- src/HomeList.js
+++ src/HomeList.js
@@ -1,13 +1,25 @@
 import React, {Component} from 'react';
+import axios from 'axios';
 
 class HomeList extends Component {
 
   constructor(props) {
     super();
 
+    this.state = {
+      homes: []
+    }
+  }
+  
+  componentWillMount() {
+    axios.get('./homes.json')
+      .then((data) => {
+        this.setState({ homes: data.data.homes })
+      });
   }
 
   render() {
+    console.log('homes: ', this.state.homes);
     return (
       <div>
         <h1>Hello World</h1>
